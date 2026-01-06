b5339e93be9babfc5284a9f77f3d0fe92c2a31b3
RoBix <malvi.rahul.@gmail.com>
2026-01-06 15:37:42 +0530

    Update hero page

--- zozo-movie-mart/tailwind.config.js
+++ zozo-movie-mart/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     extend: {
       fontFamily: {
         rubik: ['Rubik', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        akaya: ['Akaya Kanadaka', 'cursive'],
+        lilex: ['Lilex', 'sans-serif'], 
       },
     },
   },
