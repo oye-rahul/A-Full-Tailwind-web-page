72fd8f5e33ed37299dd28aa4659e61839d5a7b32
RoBix <malvi.rahul.@gmail.com>
2026-01-06 15:51:38 +0530

    My local Tailwind changes

---
+++ shoes-web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        akaya: ['Akaya Kanadaka', 'cursive'],
+        lilex: ['Lilex', 'sans-serif'], 
+      },
+    },
+  },
+}
