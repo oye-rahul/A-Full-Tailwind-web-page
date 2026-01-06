137bed08dcaf8e15ec3006aae5d1f008cdb32e08
RoBix <mamlvi.rahul.2005@gmail.com>
2026-01-06 23:36:09 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+function HandleMenu (){
+    const hidemenu = document.getElementById("hidemenu");
+    if (hidemenu) hidemenu.classList.toggle("hidden");
+}
+
+let initialTranslateLTR = 0;
+let initialTranslateRTL = 0;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting) {
+            document.addEventListener('scroll', scrollHandler);
+        } else {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback)
+
+    intersectionObserver.observe(element);
+
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
