44b652a6e165d848e6cdb471625e3f241bdb865f
RoBix <mamlvi.rahul.2005@gmail.com>
2026-01-08 03:24:22 +0530

    almost all add

--- script.js
+++ script.js
@@ -37,8 +37,10 @@ function setupIntersectionObserver(element, isLTR, speed) {
 const line1 = document.getElementById('line-1');
 const line2 = document.getElementById('line-2');
 const line3 = document.getElementById('line-3');
+const line4 = document.getElementById('line-4');
 
 // 👇 line1 & line3 ko right → left karne ke liye false set karo
 setupIntersectionObserver(line1, false, 0.15);
 setupIntersectionObserver(line2, true, 0.15);
 setupIntersectionObserver(line3, false, 0.15);
+setupIntersectionObserver(line4, true, 0.15);
